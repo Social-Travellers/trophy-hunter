e37c300fa44ade2f166e17357092a80022bbdab1
Victor Rodriguez <viictor.rodriguez@gmail.com>
2017-04-29 01:22:11 -0700

    Added UserProfileVC

---
+++ SocialTravellers/UserProfileViewController.swift
@@ -0,0 +1,40 @@
+//
+//  UserProfileViewController.swift
+//  SocialTravellers
+//
+//  Created by victor rodriguez on 4/29/17.
+//  Copyright © 2017 SocialTravellers. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userTagline: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
