eabb4bf8cd24ec786393427d9952acfe3b588e3c
Victor Rodriguez <viictor.rodriguez@gmail.com>
2017-04-28 21:40:36 -0700

    Created an EventCell file and added a var Event didSet to it. Also added colors to the EventCell in storyboard

---
+++ SocialTravellers/EventCell.swift
@@ -0,0 +1,40 @@
+//
+//  EventCell.swift
+//  SocialTravellers
+//
+//  Created by victor rodriguez on 4/28/17.
+//  Copyright © 2017 SocialTravellers. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var taglineLabel: UILabel!
+    @IBOutlet weak var userCountLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    var event: Event! {
+        didSet {
+            nameLabel.text = event.name
+            taglineLabel.text = event.tagline
+            userCountLabel.text = String(describing: event.users?.count)
+           // timeStampLabel.text =             Gotta do some date magic here.
+           // distanceLabel.text = event.location?.distance(from: <#T##CLLocation#>)    requires some location magic
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
--- SocialTravellers/EventFeedViewController.swift
+++ SocialTravellers/EventFeedViewController.swift
@@ -22,17 +22,6 @@ class EventFeedViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
@@ -43,7 +32,7 @@ extension EventFeedViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "EventTableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
         return cell
     }
     
