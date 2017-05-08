3f22d5d0510bdb0e4fe396aec98a94e2d700f3a8
Victor Rodriguez <viictor.rodriguez@gmail.com>
2017-05-07 18:04:58 -0700

    Added hamburger view
    Note: The MapView in MapViewVC takes precedence over the contentView's PanGesture. Essentially, if the user tries to pan sideways it will move the map as opposed to moving the side menu. I'm not sure how to get around that. One option would be to have a button on the MapView that shows the side menu. I think this would require a delegate since the button would be in the MapViewVC but it would be controlling the leftMarginConstraints in Containter1VC

---
+++ SocialTravellers/MenuCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuCell.swift
+//  SocialTravellers
+//
+//  Created by victor rodriguez on 5/7/17.
+//  Copyright © 2017 SocialTravellers. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
