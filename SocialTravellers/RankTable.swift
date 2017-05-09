5cdb884d30e45ec531e76a9785d92924db0a0b06
Victor Rodriguez <viictor.rodriguez@gmail.com>
2017-05-08 21:31:29 -0700

    refactored old code

---
+++ SocialTravellers/RankTable.swift
@@ -0,0 +1,33 @@
+//
+//  RankTable.swift
+//  SocialTravellers
+//
+//  Created by victor rodriguez on 5/8/17.
+//  Copyright © 2017 SocialTravellers. All rights reserved.
+//
+
+import Foundation
+
+//class RankTable: NSOBject {
+//    
+//    func lookUpRank(experiencePoints: NSNumber) -> String {
+//        
+//        if experiencePoints = experiencePoints{
+//            switch experiencePoints {
+//            case 0..<50:
+//                return "noob"
+//            case 50..<150:
+//                return "Grown-up"
+//            case 150..<1000:
+//                return "Warrior"
+//            case 1000..<5000:
+//                return "Knight"
+//            case <5000:
+//                return "Royalty"
+//            default:
+//                return "noob"
+//            }
+//        }
+//        return "noob"
+//    }
+//}
