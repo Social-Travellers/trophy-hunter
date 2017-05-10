6cd6eb626373ebee1663b8df1ded5be4ed14ef7d
Anup Kher <anup.kher.1990@gmail.com>
2017-05-09 20:21:51 -0700

    Updating all labels on trophyUnlockedVC

--- SocialTravellers/TrophyUnlockedViewController.swift
+++ SocialTravellers/TrophyUnlockedViewController.swift
@@ -12,6 +12,15 @@ import Parse
 class TrophyUnlockedViewController: UIViewController {
     var trophy: Trophy!
     
+    var user: User! {
+        didSet {
+            expToNextLevelLabel.text = rankTable.lookUpRank(experiencePoints: user.experiencePoints!)
+            currentExpLabel.text = "\(user.experiencePoints!)"
+        }
+    }
+    
+    var rankTable = RankTable()
+    
     @IBOutlet weak var trophyImageView: UIImageView!
     
     @IBOutlet weak var trophyNameLabel: UILabel!
@@ -24,6 +33,7 @@ class TrophyUnlockedViewController: UIViewController {
         super.viewDidLoad()
         
         retrieveTrophy()
+        fetchUserAndUpdateExp(userId: User.currentUser!.facebookId!)
         //updateScreenLabels()
         
         // Do any additional setup after loading the view.
@@ -35,21 +45,54 @@ class TrophyUnlockedViewController: UIViewController {
 
         query.getObjectInBackground(withId: "Mw7eCMdS0H"){ (backendTrophy: PFObject?, error: Error?) in
             if error == nil && backendTrophy != nil {
-                print(backendTrophy)
+                print(backendTrophy!)
                 self.trophy = Trophy(trophy: backendTrophy!)
                 self.updateScreenLabels()
             } else {
-                print(error)
+                print(error!)
             }
         }
     }
- 
+    
+    func fetchUserAndUpdateExp(userId facebookId: String) {
+        let query = PFQuery(className:"User1")
+        query.limit = 1; // limit to at most 1 result
+        query.whereKey("facebookId", equalTo: facebookId)
+        // Investigate if there's a query parameter that will sort this by a key-value (experience points)
+        query.findObjectsInBackground {
+            (backendUsers: [PFObject]?, error: Error?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(backendUsers!.count) users.")
+                // Do something with the found objects
+                if let backendUsers = backendUsers {
+                    let backendUser = backendUsers[0]
+                    print(backendUser.objectId ?? "")
+                    
+                    if let currentXp = backendUser["experiencePoints"] as? NSNumber {
+                        let newXp = currentXp.intValue + self.trophy.experiencePoints!.intValue
+                        backendUser["experiencePoints"] = newXp
+                        backendUser.saveInBackground() { (succeeded: Bool, error: Error?) in
+                            print("User XP updated")
+                            let frontendUser = User(PFObject: backendUser)
+                            self.user = frontendUser
+                        }
+                    }
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.localizedDescription)")
+            }
+        }
+    }
+    
     
     func updateScreenLabels(){
         trophyNameLabel.text = trophy.name!
         expAcquiredLabel.text = "\(String(describing: trophy.experiencePoints!))"
         
-        currentExpLabel.text = "\(String(describing: User.currentUser?.experiencePoints))"
+//        currentExpLabel.text = "\(String(describing: User.currentUser?.experiencePoints))"
      //  expToNextLevelLabel.text = Requires look-up table to know what exp points corresponds to what rank
         
         let userImageFile = trophy.picture!
